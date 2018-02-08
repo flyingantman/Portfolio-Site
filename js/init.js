d807e97a35fb5ff5c727df816a175e2455915319
flyingantman <flyingantman@gmail.com>
2018-02-08 01:32:10 -0500

    website

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+      $('.button-collapse').sideNav();
+      $('.parallax').parallax();
+      $('.slider').slider({full_width: true, height:500, indicators:false, transition:750, interval:8000});
+
+  }); // end of document ready
+})(jQuery);
---
+++ js/initGamedevelopment.js
@@ -0,0 +1,7 @@
+(function($){
+  $(function(){
+      $('.button-collapse').sideNav();
+      $('.parallax').parallax();
+      $('.slider').slider({full_width: true, height:500, indicators:false, transition:750, interval:8000});
+  }); // end of document ready
+})(jQuery);
